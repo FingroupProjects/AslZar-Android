6d39385d502184b0ca3e81c9c93b3c01b9bccaf0
Suhrob <s.gafurov.s00@gmail.com>
2023-07-31 09:25:07 +0500

    Initial commit

---
+++ app/src/main/java/com/fin_group/aslzar/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.fin_group.aslzar
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
