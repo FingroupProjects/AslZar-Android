4e9a305338e6c2f2f437a904822275e13110c20c
toxa2001 <107117723+toxa2001@users.noreply.github.com>
2023-08-01 14:52:09 +0500

    Bottom nav bar it working

--- app/src/main/java/com/fin_group/aslzar/ui/activities/MainActivity.kt
+++ app/src/main/java/com/fin_group/aslzar/ui/activities/MainActivity.kt
@@ -1,7 +1,14 @@
 package com.fin_group.aslzar.ui.activities
 
 import android.os.Bundle
+import android.view.Menu
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import androidx.appcompat.widget.Toolbar
+import com.fin_group.aslzar.R
 import com.fin_group.aslzar.databinding.ActivityMainBinding
 
 
@@ -13,5 +20,27 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        val toolbar: Toolbar = findViewById(R.id.id)
+        setSupportActionBar(toolbar)
+
+
+        val bottomNavigationView = binding.bottomNavigationView
+        val navController =findNavController(R.id.fragmentMain)
+
+        val appBarConfiguration = AppBarConfiguration(setOf(
+            R.id.mainFragment, R.id.mainCartFragment
+        ))
+
+        setupActionBarWithNavController(navController,appBarConfiguration)
+        bottomNavigationView.setupWithNavController(navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.fragmentMain)
+        return navController.navigateUp() || super.onSupportNavigateUp()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        return super.onCreateOptionsMenu(menu)
     }
 }
