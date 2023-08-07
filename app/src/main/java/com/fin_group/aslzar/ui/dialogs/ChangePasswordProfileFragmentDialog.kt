9511a5d5a677055fe075352669d694a067b0ad94
toxa2001 <107117723+toxa2001@users.noreply.github.com>
2023-08-07 17:07:47 +0500

    profileChange

--- app/src/main/java/com/fin_group/aslzar/ui/dialogs/ChangePasswordProfileFragmentDialog.kt
+++ app/src/main/java/com/fin_group/aslzar/ui/dialogs/ChangePasswordProfileFragmentDialog.kt
@@ -1,17 +1,32 @@
 package com.fin_group.aslzar.ui.dialogs
 
+import android.content.res.ColorStateList
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
+import android.widget.ProgressBar
 import android.widget.Toast
+import androidx.core.content.ContextCompat
+import com.fin_group.aslzar.R
 import com.fin_group.aslzar.databinding.FragmentDialogChangePasswordProfileBinding
 import com.fin_group.aslzar.util.BaseDialogFragment
+import com.fin_group.aslzar.util.hideBottomNav
+import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.textfield.TextInputLayout
+import com.nulabinc.zxcvbn.Zxcvbn
 
 class ChangePasswordProfileFragmentDialog : BaseDialogFragment() {
 
     private var _binding: FragmentDialogChangePasswordProfileBinding? = null
     private val binding get() = _binding!!
+
+    private lateinit var progressBar: ProgressBar
+    private lateinit var newPasswordInputLayout: TextInputLayout
+    private lateinit var newPasswordEditText: TextInputEditText
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -19,12 +34,40 @@ class ChangePasswordProfileFragmentDialog : BaseDialogFragment() {
     ): View? {
         _binding = FragmentDialogChangePasswordProfileBinding.inflate(inflater, container, false)
 
+        progressBar = binding.progressBar2
+        newPasswordInputLayout = binding.newPasswordChange
+        newPasswordEditText = binding.editNewPasswordChange
 
         passwordCheck()
+
+        newPasswordEditText.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
+
+            override fun afterTextChanged(s: Editable?) {
+                val newPassword = s?.toString() ?: ""
+                updateProgressBar(newPassword)
+            }
+        })
 
         return binding.root
     }
 
+    private fun updateProgressBar(password: String) {
+        val zxcvbn = Zxcvbn()
+        val result = zxcvbn.measure(password)
+        val score = result.score
+        progressBar.progress = score
+
+        when (score) {
+            0 -> progressBar.progressTintList = ColorStateList.valueOf(ContextCompat.getColor(requireContext(), R.color.progressColorVeryWeak))
+            1 -> progressBar.progressTintList = ColorStateList.valueOf(ContextCompat.getColor(requireContext(), R.color.progressColorWeak))
+            2 -> progressBar.progressTintList = ColorStateList.valueOf(ContextCompat.getColor(requireContext(), R.color.progressColorMedium))
+            3 -> progressBar.progressTintList = ColorStateList.valueOf(ContextCompat.getColor(requireContext(), R.color.progressColorStrong))
+            4 -> progressBar.progressTintList = ColorStateList.valueOf(ContextCompat.getColor(requireContext(), R.color.progressColorVeryStrong))
+        }
+    }
 
 
 
@@ -42,11 +85,6 @@ class ChangePasswordProfileFragmentDialog : BaseDialogFragment() {
 
             if (oldPasswordText.isEmpty()){
                 binding.editOldPasswordChange.error = "Введите старый пароль!"
-                binding.editOldPasswordChange.requestFocus()
-                return@setOnClickListener
-            }
-            else if (oldPasswordText.length < 8){
-                binding.editOldPasswordChange.error = "Минимальная длина паролья 8 символов"
                 binding.editOldPasswordChange.requestFocus()
                 return@setOnClickListener
             }
@@ -80,5 +118,9 @@ class ChangePasswordProfileFragmentDialog : BaseDialogFragment() {
         binding.floatingActionButton.setOnClickListener {
             dismiss()
         }
+    }
+    override fun onStart() {
+        super.onStart()
+        hideBottomNav()
     }
 }
