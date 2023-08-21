b3426a12e293d821f00a2fc88500ceafeff89c03
toxa2001 <107117723+toxa2001@users.noreply.github.com>
2023-08-21 17:08:04 +0500

    calculator

---
+++ app/src/main/java/com/fin_group/aslzar/models/AllClientType.kt
@@ -0,0 +1,6 @@
+package com.fin_group.aslzar.models
+
+data class AllClientType(
+    val id: Int,
+    val name: String
+)
--- app/src/main/java/com/fin_group/aslzar/ui/fragments/cartMain/cart/functions/CartFragmentFunctions.kt
+++ app/src/main/java/com/fin_group/aslzar/ui/fragments/cartMain/cart/functions/CartFragmentFunctions.kt
@@ -1,12 +1,13 @@
 package com.fin_group.aslzar.ui.fragments.cartMain.cart.functions
 
-import android.util.Log
+import android.annotation.SuppressLint
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.fin_group.aslzar.adapter.ProductInCartAdapter
 import com.fin_group.aslzar.models.ProductInCart
 import com.fin_group.aslzar.ui.dialogs.DeleteAllProductFromCartFragmentDialog
 import com.fin_group.aslzar.ui.fragments.cartMain.cart.CartFragment
 
+@SuppressLint("NotifyDataSetChanged")
 fun CartFragment.fetchRV(productInCartList: List<ProductInCart>){
     recyclerView.layoutManager = LinearLayoutManager(requireContext())
     myAdapter = ProductInCartAdapter(productInCartList, this)
