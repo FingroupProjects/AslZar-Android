ed11ea3aa3c03cda1e48fdb5483703570337b188
Suhrob <s.gafurov.s00@gmail.com>
2023-08-25 11:02:05 +0500

    QR code scanner is working

--- app/src/main/java/com/fin_group/aslzar/ui/fragments/barCode/BarCodeScannerFragment.kt
+++ app/src/main/java/com/fin_group/aslzar/ui/fragments/barCode/BarCodeScannerFragment.kt
@@ -59,83 +59,83 @@ class BarCodeScannerFragment : Fragment() {
         ) {
             askForCameraPermission()
         } else {
-            setupControls()
+            //setupControls()
         }
         val aniSlide: Animation =
             AnimationUtils.loadAnimation(requireContext(), R.anim.scanner_animation)
         binding.barcodeLine.startAnimation(aniSlide)
     }
 
-    private fun setupControls() {
-        barcodeDetector =
-            BarcodeDetector.Builder(requireContext()).setBarcodeFormats(Barcode.ALL_FORMATS).build()
-
-        cameraSource = CameraSource.Builder(requireContext(), barcodeDetector)
-            .setRequestedPreviewSize(1920, 1080)
-            .setAutoFocusEnabled(true) //you should add this feature
-            .build()
-
-        binding.cameraSurfaceView.getHolder().addCallback(object : SurfaceHolder.Callback {
-            @SuppressLint("MissingPermission")
-            override fun surfaceCreated(holder: SurfaceHolder) {
-                try {
-                    //Start preview after 1s delay
-                    cameraSource.start(holder)
-                } catch (e: IOException) {
-                    e.printStackTrace()
-                }
-            }
-
-            @SuppressLint("MissingPermission")
-            override fun surfaceChanged(
-                holder: SurfaceHolder,
-                format: Int,
-                width: Int,
-                height: Int
-            ) {
-                try {
-                    cameraSource.start(holder)
-                } catch (e: IOException) {
-                    e.printStackTrace()
-                }
-            }
-
-            override fun surfaceDestroyed(holder: SurfaceHolder) {
-                cameraSource.stop()
-            }
-        })
-
-
-        try {
-            barcodeDetector.setProcessor(object : Detector.Processor<Barcode> {
-                override fun release() {
-                    Log.d("TAG", "release: Scanner has been closed")
-                }
-                override fun receiveDetections(detections: Detector.Detections<Barcode>) {
-                    val barcodes = detections.detectedItems
-                    if (barcodes.size() == 1) {
-                        scannedValue = barcodes.valueAt(0).rawValue
-                        try {
-                            activity?.runOnUiThread {
-                                cameraSource.stop()
-                                val action =
-                                    BarCodeScannerFragmentDirections.actionBarCodeScannerFragmentToDataProductFragment(
-                                        scannedValue.toString()
-                                    )
-                                Navigation.findNavController(binding.root).navigate(action)
-                            }
-                        }catch (e: Exception){
-                            Log.d("TAG", "receiveDetections: ${e.message}")
-                        }
-                    } else {
-                        Log.d("TAG", "receiveDetections: Values not detected")
-                    }
-                }
-            })
-        }catch (e: Exception){
-            Log.d("TAG", "setupControls: ${e.message}")
-        }
-    }
+//    private fun setupControls() {
+//        barcodeDetector =
+//            BarcodeDetector.Builder(requireContext()).setBarcodeFormats(Barcode.ALL_FORMATS).build()
+//
+//        cameraSource = CameraSource.Builder(requireContext(), barcodeDetector)
+//            .setRequestedPreviewSize(1920, 1080)
+//            .setAutoFocusEnabled(true) //you should add this feature
+//            .build()
+//
+//        binding.cameraSurfaceView.getHolder().addCallback(object : SurfaceHolder.Callback {
+//            @SuppressLint("MissingPermission")
+//            override fun surfaceCreated(holder: SurfaceHolder) {
+//                try {
+//                    //Start preview after 1s delay
+//                    cameraSource.start(holder)
+//                } catch (e: IOException) {
+//                    e.printStackTrace()
+//                }
+//            }
+//
+//            @SuppressLint("MissingPermission")
+//            override fun surfaceChanged(
+//                holder: SurfaceHolder,
+//                format: Int,
+//                width: Int,
+//                height: Int
+//            ) {
+//                try {
+//                    cameraSource.start(holder)
+//                } catch (e: IOException) {
+//                    e.printStackTrace()
+//                }
+//            }
+//
+//            override fun surfaceDestroyed(holder: SurfaceHolder) {
+//                cameraSource.stop()
+//            }
+//        })
+//
+//
+//        try {
+//            barcodeDetector.setProcessor(object : Detector.Processor<Barcode> {
+//                override fun release() {
+//                    Log.d("TAG", "release: Scanner has been closed")
+//                }
+//                override fun receiveDetections(detections: Detector.Detections<Barcode>) {
+//                    val barcodes = detections.detectedItems
+//                    if (barcodes.size() == 1) {
+//                        scannedValue = barcodes.valueAt(0).rawValue
+//                        try {
+//                            activity?.runOnUiThread {
+//                                cameraSource.stop()
+//                                val action =
+//                                    BarCodeScannerFragmentDirections.actionBarCodeScannerFragmentToDataProductFragment(
+//                                        scannedValue.toString()
+//                                    )
+//                                Navigation.findNavController(binding.root).navigate(action)
+//                            }
+//                        }catch (e: Exception){
+//                            Log.d("TAG", "receiveDetections: ${e.message}")
+//                        }
+//                    } else {
+//                        Log.d("TAG", "receiveDetections: Values not detected")
+//                    }
+//                }
+//            })
+//        }catch (e: Exception){
+//            Log.d("TAG", "setupControls: ${e.message}")
+//        }
+//    }
 
     private fun askForCameraPermission() {
         ActivityCompat.requestPermissions(
@@ -145,20 +145,20 @@ class BarCodeScannerFragment : Fragment() {
         )
     }
 
-    override fun onRequestPermissionsResult(
-        requestCode: Int,
-        permissions: Array<out String>,
-        grantResults: IntArray
-    ) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        if (requestCode == requestCodeCameraPermission && grantResults.isNotEmpty()) {
-            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                setupControls()
-            } else {
-                Toast.makeText(requireContext(), "Permission Denied", Toast.LENGTH_SHORT).show()
-            }
-        }
-    }
+//    override fun onRequestPermissionsResult(
+//        requestCode: Int,
+//        permissions: Array<out String>,
+//        grantResults: IntArray
+//    ) {
+//        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+//        if (requestCode == requestCodeCameraPermission && grantResults.isNotEmpty()) {
+//            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+//                setupControls()
+//            } else {
+//                Toast.makeText(requireContext(), "Permission Denied", Toast.LENGTH_SHORT).show()
+//            }
+//        }
+//    }
 
     override fun onStart() {
         super.onStart()
